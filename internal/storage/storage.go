a041a28f61b95efddd686a3afe1013fa6022e6c1
Roman <47863880+lionslon@users.noreply.github.com>
2023-09-12 14:31:38 +0300

    Revert "Increment 3 is done"

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,10 +1,5 @@
 package storage
 
-import (
-	"fmt"
-	"net/http"
-)
-
 type gauge float64
 type counter int64
 
@@ -26,32 +21,4 @@ func (s *MemStorage) UpdateCounter(n string, v int64) {
 
 func (s *MemStorage) UpdateGauge(n string, v float64) {
 	s.gaugeData[n] = gauge(v)
-}
-
-func (s *MemStorage) GetValue(t string, n string) (string, int) {
-	var v string
-	statusCode := http.StatusOK
-	if val, ok := s.gaugeData[n]; ok && t == "gauge" {
-		v = fmt.Sprint(val)
-	} else if val, ok := s.counterData[n]; ok && t == "counter" {
-		v = fmt.Sprint(val)
-	} else {
-		statusCode = http.StatusNotFound
-	}
-	return v, statusCode
-}
-
-func (s *MemStorage) AllMetrics() string {
-	var result string
-	result += "Gauge metrics:\n"
-	for n, v := range s.gaugeData {
-		result += fmt.Sprintf("- %s = %f\n", n, v)
-	}
-
-	result += "Counter metrics:\n"
-	for n, v := range s.counterData {
-		result += fmt.Sprintf("- %s = %d\n", n, v)
-	}
-
-	return result
 }
