dae71b09c6e0f49771089dacd133ae4f9e477326
Roman <47863880+lionslon@users.noreply.github.com>
2023-09-12 14:29:16 +0300

    Revert "Increment 5 is done"

--- internal/api/api.go
+++ internal/api/api.go
@@ -7,7 +7,6 @@ import (
 	"github.com/lionslon/yapmetrics/internal/handlers"
 	"github.com/lionslon/yapmetrics/internal/storage"
 	"log"
-	"os"
 )
 
 type APIServer struct {
@@ -20,14 +19,9 @@ func New() *APIServer {
 	apiS := &APIServer{}
 	apiS.storage = storage.New()
 	apiS.echo = echo.New()
-	var address string
-	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
-		address = envRunAddr
-	} else {
-		flag.StringVar(&address, "a", "localhost:8080", "address and port to run server")
-		flag.Parse()
-	}
-	apiS.addr = address
+	address := flag.String("a", "localhost:8080", "address and port to run server")
+	flag.Parse()
+	apiS.addr = *address
 
 	apiS.echo.GET("/", handlers.AllMetrics(apiS.storage))
 	apiS.echo.GET("/value/:typeM/:nameM", handlers.MetricsValue(apiS.storage))
